a178ffba825e429962103d380c050ebd7a8c3ec8
daksh <dakshsharda1@gmail.com>
2021-10-16 22:25:35 +0530

    update

--- ui/src/App.test.js
+++ ui/src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
+import React from 'react';
+import ReactDom from 'react-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('App render check', () => {
+  const div = document.createElement("div");
+  render(<App />, div);
+  ReactDom.unmountComponentAtNode(div);
 });
